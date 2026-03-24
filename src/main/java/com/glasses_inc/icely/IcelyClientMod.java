d0689c0ed0e7ddda1c4dae766f4f8b142e6c2509
Salmon228 <salmon228@inbox.ru>
2026-03-24 17:12:09 +0300

    Starting commit

---
+++ src/main/java/com/glasses_inc/icely/IcelyClientMod.java
@@ -0,0 +1,10 @@
+package com.glasses_inc.icely;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IcelyClientMod implements ClientModInitializer  {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/glasses_inc/icely/IcelyDataGenerator.java
@@ -0,0 +1,11 @@
+package com.glasses_inc.icely;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IcelyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
