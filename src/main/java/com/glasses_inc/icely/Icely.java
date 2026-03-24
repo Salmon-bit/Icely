0d605d4feee2a42d52e7e1b856531b4c62786344
Salmon-bit <salmon228@inbox.ru>
2026-03-24 21:05:08 +0300

    Added two items Items added: - Icicle - Blue ice icicle

--- src/main/java/com/glasses_inc/icely/Icely.java
+++ src/main/java/com/glasses_inc/icely/Icely.java
@@ -1,5 +1,6 @@
 package com.glasses_inc.icely;
 
+import com.glasses_inc.icely.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,5 +12,6 @@ public class Icely implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerItems();
 	}
 }
---
+++ src/main/java/com/glasses_inc/icely/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.glasses_inc.icely.item;
+
+import com.glasses_inc.icely.Icely;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item BLUE_ICE_ICICLE = registerItem("blue_ice_icicle", new Item(new Item.Settings()));
+    public static final Item ICICLE = registerItem("icicle", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return  Registry.register(Registries.ITEM, Identifier.of(Icely.MOD_ID, name), item);
+    }
+
+    public static void registerItems() {
+        Icely.LOGGER.info("Registering ModItems for " + Icely.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
+            entries.add(ICICLE);
+            entries.add(BLUE_ICE_ICICLE);
+        });
+
+    }
+}
